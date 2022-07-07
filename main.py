1c3ea7a9cf31c759bee3a34cbfe4f708ac631643
Grimmys <funkyzermmy@gmail.com>
2022-07-07 22:29:27 +0200

    Use screen sizes constants

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pygame
 
-from src.constants import GAME_TITLE
+from main_menu import MainMenu
+from src.constants import GAME_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT
 
 if __name__ == "__main__":
     pygame.init()
     pygame.display.set_caption(GAME_TITLE)
-    screen = pygame.display.set_mode((1280, 720))
-    from main_menu import MainMenu
-    mainMenu = MainMenu(screen)
-    mainMenu.run()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    main_menu = MainMenu(screen)
+    main_menu.run()
