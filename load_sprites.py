6b1e94f47ca9dbc68d6bdd86f9f00057d24db9e5
ziyinw3 <ziyinw3@illinois.edu>
2022-07-06 19:17:20 -0500

    upload all sprites and unabbreviate

--- load_sprites.py
+++ load_sprites.py
@@ -25,82 +25,98 @@ class SpriteSheet:
 # load in
 mc_ss = SpriteSheet('assets\mc_ss.png')
 misc_ss = SpriteSheet('assets\misc_ss.png')
+lit_buttons = SpriteSheet('assets\lit_buttons.png')
 
 # right facing idle
-mc_ri = []
-mc_ri_coords = []
+mc_right_idle = []
+mc_right_idle_coords = []
 for i in range(0, 7):
-    mc_ri_coords.append((i * 24, 0, i * 24 + 24, 30))
-# print(mc_ri_coords)
-mc_ss.load_strip(mc_ri_coords, mc_ri, 24, 30)
+    mc_right_idle_coords.append((i * 96, 0, i * 96 + 96, 480))
+# print(mc_right_idle_coords)
+mc_ss.load_strip(mc_right_idle_coords, mc_right_idle, 96, 480)
 
 # right facing walk
-mc_rw = []
-mc_rw_coords = []
+mc_right_walk = []
+mc_right_walk_coords = []
 for i in range(0, 4):
-    mc_rw_coords.append((i * 24, 30, i * 24 + 24, 60))
-# print(mc_rw_coords)
-mc_ss.load_strip(mc_rw_coords, mc_rw, 24, 30)
+    mc_right_walk_coords.append((i * 96, 480, i * 96 + 96, 240))
+# print(mc_right_walk_coords)
+mc_ss.load_strip(mc_right_walk_coords, mc_right_walk, 96, 480)
 
 # right jump
-mc_rj_rect = (0, 60, 24, 90)
-mc_rj = mc_ss.image_at(mc_rj_rect, 24, 30)
+mc_right_jump_rect = (0, 240, 96, 360)
+mc_right_jump = mc_ss.image_at(mc_right_jump_rect, 96, 480)
 
 # right interact
-mc_r_interact_rect = (24, 60, 48, 90)
-mc_r_interact = mc_ss.image_at(mc_r_interact_rect, 24, 30)
+mc_r_interact_rect = (96, 240, 192, 360)
+mc_r_interact = mc_ss.image_at(mc_r_interact_rect, 96, 480)
 
 #----------------------------------------------------------------
 
 # left idle
-mc_li = []
-mc_li_coords = []
+mc_left_idle = []
+mc_left_idle_coords = []
 for i in range(0, 7):
-    mc_li_coords.append((i * 24, 90, i * 24 + 24, 120))
-# print (mc_li_coords)
-mc_ss.load_strip(mc_li_coords, mc_li, 24, 30)
+    mc_left_idle_coords.append((i * 96, 360, i * 96 + 96, 480))
+# print (mc_left_idle_coords)
+mc_ss.load_strip(mc_left_idle_coords, mc_left_idle, 96, 480)
 
 # left facing walk
-mc_lw = []
-mc_lw_coords = []
+mc_left_walk = []
+mc_left_walk_coords = []
 for i in range(0, 4):
-    mc_lw_coords.append((i * 24, 120, i * 24 + 24, 150))
-mc_ss.load_strip(mc_lw_coords, mc_lw, 24, 30)
+    mc_left_walk_coords.append((i * 96, 480, i * 96 + 96, 600))
+mc_ss.load_strip(mc_left_walk_coords, mc_left_walk, 96, 480)
 
 # left jump
-mc_lj_rect = (24, 150, 48, 180)
-mc_lj = mc_ss.image_at(mc_lj_rect, 24, 30)
+mc_lj_rect = (96, 600, 192, 780)
+mc_lj = mc_ss.image_at(mc_lj_rect, 96, 480)
 
 # left interact
-mc_l_interact_rect = (0, 150, 24, 180)
-mc_l_interact = mc_ss.image_at(mc_l_interact_rect, 24, 30)
+mc_l_interact_rect = (0, 600, 96, 780)
+mc_l_interact = mc_ss.image_at(mc_l_interact_rect, 96, 480)
 
 #------------------------------------------------------
 
 # death
-mc_death_rect = (0, 180, 24, 210)
-mc_death = mc_ss.image_at(mc_death_rect, 24, 30)
+mc_death_rect = (0, 780, 96, 840)
+mc_death = mc_ss.image_at(mc_death_rect, 96, 480)
 
 #------------------------------------------------------
 
 # papier-san
 
-pp = []
-pp_coords = []
+paper = []
+paper_coords = []
 for i in range(0, 3):
-    pp_coords.append((i * 20, 0, i * 20 + 20, 24))
-misc_ss.load_strip(pp_coords, pp, 20, 24)
+    paper_coords.append((i * 80, 0, i * 80 + 80, 96))
+misc_ss.load_strip(paper_coords, paper, 80, 96)
 
 #CMY toggler, c - 0, m - 1, y - 2
 
 toggles = []
 toggles_coords = []
 for i in range(0, 3):
-    toggles_coords.append((i * 50, 24, i * 50 + 50, 24 + 53))
-misc_ss.load_strip(toggles_coords, toggles, 50, 53)
+    toggles_coords.append((i * 200, 96, i * 200 + 200, 96 + 212))
+misc_ss.load_strip(toggles_coords, toggles, 200, 212)
 
 # cat blob
-
 enemy = []
+enemy_coords = []
+for i in range(0, 2):
+    enemy_coords.append((i * 480, 212 + 96, i * 480 + 480, 212 + 96 + 80))
+misc_ss.load_strip(enemy_coords, enemy, 480, 80)
 
+# cartridges, c - 0, m - 1, y - 2
+cartridges = []
+cartridge_coords = []
+for i in range(0, 3):
+    cartridge_coords.append((i * 23, 212 + 96 + 80, i * 23 + 23, 212 + 96 + 80 + 21))
+misc_ss.load_strip(cartridge_coords, cartridges, 23, 21)
 
+# main menu buttons
+start_button_rect = (0, 0, 568, 112)
+start_button = lit_buttons.image_at(start_button_rect, 568, 112)
+
+settings_button_rect = (0, 112, 568, 46)
+settings_button = lit_buttons.image_at(settings_button_rect, 568, 112)
