1102afefd48921719c6f479969e254623261d087
Grimmys <funkyzermmy@gmail.com>
2022-07-11 23:18:49 +0200

    Add working close button for settings pop-up

--- src/scenes/main_menu.py
+++ src/scenes/main_menu.py
@@ -15,26 +15,34 @@ MAIN_MENU_BACKGROUND = pygame.image.load("assets/images/mm.png")
 
 
 class MainMenu(Scene):
+    MUSIC_ALREADY_STARTED = False
+
     def __init__(self, screen):
         super().__init__(screen)
-        self.start_button = Button(start_button_img, screen, 356, 404, 568, 112)
-        self.settings_button = Button(settings_button_img, screen, 356, 528, 568, 112)
+        self.buttons = [Button(start_button_img, screen, 356, 404, 568, 112, Tutorial),
+                        Button(settings_button_img, screen, 356, 528, 568, 112, Settings)]
 
     def run(self):
         super().run()
-        pygame.mixer.music.load("assets/sounds/bg_track.mp3")
-        pygame.mixer.music.set_volume(0.4)
-        pygame.mixer.music.play(-1)
+        if not MainMenu.MUSIC_ALREADY_STARTED:
+            pygame.mixer.music.load("assets/sounds/bg_track.mp3")
+            pygame.mixer.music.set_volume(0.4)
+            pygame.mixer.music.play(-1)
+            MainMenu.MUSIC_ALREADY_STARTED = True
 
     def draw(self):
         super().draw()
         self.screen.blit(MAIN_MENU_BACKGROUND, (0, 0))
-        self.start_button.detect_hover()
-        self.settings_button.detect_hover()
+        for button in self.buttons:
+            button.detect_hover()
 
     def process_event(self, event: pygame.event.Event):
         super().process_event(event)
         if event.type == pygame.MOUSEBUTTONDOWN:
-            self.next_scene = self.start_button.navigate(Tutorial)
-            if self.next_scene is None:
-                self.next_scene = self.settings_button.navigate(Settings)
+            for button in self.buttons:
+                if button.rect.collidepoint(pygame.mouse.get_pos()):
+                    self.next_scene = button.linked_scene(self.screen)
+                    if isinstance(self.next_scene, Settings):
+                        self.next_scene.scene_bound_to_close_button = MainMenu
+                    self.next_scene.run()
+                    break
--- src/scenes/settings.py
+++ src/scenes/settings.py
@@ -1,14 +1,32 @@
 import pygame
 
+from src.gui.button import Button
 from src.scenes.scene import Scene
 
-SETTINGS_BACKGROUND = pygame.image.load("assets/images/settings_pop_up.png")
+SETTINGS_SCENE_IMAGE = pygame.image.load("assets/images/settings_pop_up.png")
+CLOSE_BUTTON_IMAGE = pygame.image.load("assets/images/cross.png")
+CLOSE_BUTTON_DISTANCE_TO_BORDER = 20
 
 
 class Settings(Scene):
     def __init__(self, screen: pygame.Surface):
         super().__init__(screen)
+        self.position = (self.screen.get_width() // 2 - SETTINGS_SCENE_IMAGE.get_width() // 2,
+                         self.screen.get_height() // 2 - SETTINGS_SCENE_IMAGE.get_height() // 2)
+        self.close_button = Button(CLOSE_BUTTON_IMAGE, screen,
+                                   SETTINGS_SCENE_IMAGE.get_width() - CLOSE_BUTTON_IMAGE.get_width() -
+                                   CLOSE_BUTTON_DISTANCE_TO_BORDER + self.position[0],
+                                   CLOSE_BUTTON_DISTANCE_TO_BORDER + self.position[1],
+                                   CLOSE_BUTTON_IMAGE.get_width(), CLOSE_BUTTON_IMAGE.get_height())
+        self.scene_bound_to_close_button = None
 
     def draw(self):
         super().draw()
-        self.screen.blit(SETTINGS_BACKGROUND, (0, 0))
+        self.screen.blit(SETTINGS_SCENE_IMAGE, self.position)
+        self.screen.blit(self.close_button.image, self.close_button.rect)
+
+    def process_event(self, event: pygame.event.Event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.close_button.rect.collidepoint(pygame.mouse.get_pos()):
+                self.next_scene = self.scene_bound_to_close_button(self.screen)
+                self.next_scene.run()
