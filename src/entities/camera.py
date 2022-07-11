a0f8d0edb7a109a0b60260312248d260b1dc6103
ziyinw3 <ziyinw3@illinois.edu>
2022-07-11 11:04:33 -0500

    "implement" box camera

--- src/entities/camera.py
+++ src/entities/camera.py
@@ -1,7 +1,45 @@
 # camera
 
+import pygame
 from src.entities.entity import Entity
 
+vec =  pygame.math.Vector2
+
 class Camera(Entity):
-    def __init__(self, x_coord, y_coord, width, height):
+    def __init__(self, x_coord, y_coord, width, height, screen):
         super().__init__(x_coord, y_coord, width, height)
+        self.screen = screen
+        
+        self.offset = vec(0, 0)
+        self.half_w = 640
+        self.half_h = 360
+
+        # self.camera_borders = {'left': 200, 'right': 200, 'top': 100, 'bottom': 100}
+        left = 200
+        top = 100
+        width = 1280 - 400
+        height = 720 - 200
+        self.camera_rect = pygame.Rect(left, top, width, height)
+    
+    def box_target_camera(self, target):
+
+        if target.rect.left < self.camera_rect.left:
+            self.camera_rect.left = target.rect.left
+        if target.rect.right > self.camera_rect.right:
+            self.camera_rect.right = target.rect.right
+        if target.rect.top < self.camera_rect.top:
+            self.camera_rect.top = target.rect.top
+        if target.rect.bottom > self.camera_rect.bottom:
+            self.camera_rect.bottom = target.rect.bottom
+
+        self.offset.x = self.camera_rect.left - 200
+        self.offset.y = self.camera_rect.top - 100
+
+    def custom_draw(self, player, moving_entities):
+    
+        self.box_target_camera(player)
+
+        # active elements
+        for entity in moving_entities:
+            offset_pos = entity.rect.topleft - self.offset
+            self.screen.blit(entity, offset_pos)
